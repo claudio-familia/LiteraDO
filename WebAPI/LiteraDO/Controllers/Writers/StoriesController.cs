894bd25dafe35f03509503c934273cb03fede568
Claudio Jr. Familia Morel <claudio.familia.morel@gmail.com>
2023-08-10 15:32:35 -0400

    Update

--- WebAPI/LiteraDO/Controllers/Writers/StoriesController.cs
+++ WebAPI/LiteraDO/Controllers/Writers/StoriesController.cs
@@ -6,6 +6,7 @@ using LiteraDO.BusinessLogic.Services.Writers;
 using LiteraDO.Common.Services.Contracts;
 using LiteraDO.Controllers.Base;
 using LiteraDO.DataAccess.Repositories.Contracts;
+using LiteraDO.Domain.Readers;
 using LiteraDO.Domain.Writers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -22,12 +23,18 @@ namespace LiteraDO.Controllers
         private readonly IStoryService baseService;
         private readonly IBlobStorageService storageService;
         private readonly ICurrentUserService currentUserService;
+        private readonly IDataRepository<StoryUserState> storyUserStateRepository;
 
-        public StoriesController(IStoryService baseService, IBlobStorageService StorageService, ICurrentUserService currentUserService) : base(baseService)
+        public StoriesController(
+            IStoryService baseService, 
+            IBlobStorageService StorageService, 
+            ICurrentUserService currentUserService,
+            IDataRepository<StoryUserState> storyUserStateRepository) : base(baseService)
         {
             this.baseService = baseService;
             storageService = StorageService;
             this.currentUserService = currentUserService;
+            this.storyUserStateRepository = storyUserStateRepository;
         }
 
         [Route("{id}/publish")]
@@ -87,11 +94,58 @@ namespace LiteraDO.Controllers
                 storageService.UploadDocument(fileName, stream);
                 _story.Cover = $"https://literadostorage.blob.core.windows.net/literado/{fileName}";
             }
+            else
+            {
+                var savedStory = baseService.GetAll().FirstOrDefault(x => x.Id == _story.Id);
+                _story.Cover = savedStory.Cover;
+            }
 
             var result = baseService.Update(_story);
 
 
             return Ok(result);
+        }
+
+        [Route("{id}/chapterinfo/mark-read")]
+        [HttpPost]
+        public IActionResult MarkAsRead(int id)
+        {
+            var entity = baseService.Get(x => x, x => x.Id == id);
+
+            var response = storyUserStateRepository.Add(new StoryUserState()
+            {
+                StoryId = id,
+                UserId = currentUserService.UserId.Value,
+                Rating = 0,
+                StartDate = DateTime.Now,
+                ChapterWhereTheyLeft = "", 
+                RatingComment = ""
+            });
+
+            return Ok(response);
+        }
+
+        [Route("{id}/chapterinfo/chapter/{chapterId}")]
+        [HttpPost]
+        public IActionResult SaveChapter(int id, int chapterId)
+        {
+            var entity = storyUserStateRepository.GetAll(x => x).FirstOrDefault(x => x.Id == id);
+
+            entity.ChapterWhereTheyLeft = chapterId.ToString();
+
+            var response = storyUserStateRepository.Update(entity);
+
+            return Ok(response);
+        }
+
+
+        [Route("{id}/chapterinfo")]
+        [HttpGet]
+        public IActionResult GetChapterInfo(int id)
+        {
+            var entity = storyUserStateRepository.GetAll(x => x).FirstOrDefault(x => x.StoryId == id && x.UserId == currentUserService.UserId.Value);
+
+            return Ok(entity);
         }
     }
 }
--- WebApp/src/app/core/components/sidenav/sidenav.component.ts
+++ WebApp/src/app/core/components/sidenav/sidenav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuItem } from '../../models/menu-item';
 import { HttpClient } from '@angular/common/http';
+import { UserService } from 'src/app/features/auth/services/user.service';
 
 @Component({
   selector: 'literado-sidenav',
@@ -9,12 +10,27 @@ import { HttpClient } from '@angular/common/http';
 })
 export class SidenavComponent implements OnInit {
   menu: MenuItem[] = [];
+  role: number = 2;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private userService: UserService) { }
 
   ngOnInit(): void {
+    this.userService.getInformation().subscribe(res => {
+      this.role = Number(res.targetPreference)
+    });
     this.http.get<MenuItem[]>("assets/menus/sidenav.json").subscribe(data => {
       this.menu = data;
     });
+  }
+
+  validate(option: any): boolean {
+
+    if (this.role === 2) return true;
+
+    if (option === 2) return true;
+
+    if(option === this.role) return true;
+
+    return false;
   }
 }
